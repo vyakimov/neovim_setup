10b587d794630ec286b7dc0b2ecdd74cdab157de
Victor Yakimov <vya@isynet.net>
2025-08-18 11:06:11 +0200

    removed bulid opt

--- lua/plugins/vectorcode.lua
+++ lua/plugins/vectorcode.lua
@@ -1,6 +1,5 @@
 return {
   "Davidyz/VectorCode",
-  build = "uv tool run --from=vectorcode vectorcode --build",
   ft = { "python", "lua", "javascript", "typescript", "rust", "go" }, -- Add your languages
   opts = {
     -- Optional: configure async backend
