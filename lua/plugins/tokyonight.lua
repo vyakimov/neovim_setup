8aca41e22bed1da1b36fd1171a87f6172d11ad08
Victor Yakimov <vya@isynet.net>
2025-09-17 12:18:17 +0200

    orange splits

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,6 +1,14 @@
+-- ~/.config/nvim/lua/plugins/tokyonight.lua
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    on_highlights = function(hl, c)
+      local color = "#F1A368" -- You can change this hex code to any color you like
+      -- Set the color for both vertical and horizontal split separators
+      hl.VertSplit = { fg = color }
+      hl.WinSeparator = { fg = color }
+    end,
+  },
 }
