d5284615c672c7f3f319c5d22967b89ce1ea5508
Victor Yakimov <vya@isynet.net>
2026-02-11 11:02:48 +0100

    glance plugin

---
+++ lua/plugins/glance.lua
@@ -0,0 +1,10 @@
+return {
+  "dnlhc/glance.nvim",
+  cmd = "Glance",
+  keys = {
+    { "gpd", "<cmd>Glance definitions<cr>", desc = "Peek definition" },
+    { "gpr", "<cmd>Glance references<cr>", desc = "Peek references" },
+    { "gpt", "<cmd>Glance type_definitions<cr>", desc = "Peek type def" },
+  },
+  opts = {},
+}
