7f57539f0350ec5af8278bf860109a2cc462cc8e
Victor Yakimov <vya@isynet.net>
2026-02-11 11:17:26 +0100

    yanking over ssh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,38 +2,41 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local function setup_clipboard()
+  local is_remote = os.getenv("SSH_CONNECTION") ~= nil or os.getenv("SSH_CLIENT") ~= nil
   local in_container = os.getenv("CONTAINER")
     or os.getenv("DOCKER_CONTAINER")
     or vim.fn.filereadable("/.dockerenv") == 1
-  if in_container and vim.env.TMUX then
-    vim.g.clipboard = {
-      name = "OSC 52 + tmux",
-      copy = {
-        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-      },
-      paste = {
-        ["+"] = { "tmux", "save-buffer", "-" },
-        ["*"] = { "tmux", "save-buffer", "-" },
-      },
-    }
-  elseif in_container then
-    -- OSC52 copy-only if no tmux
-    vim.g.clipboard = {
-      name = "OSC 52 (copy only)",
-      copy = {
-        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-      },
-      paste = {
-        ["+"] = function()
-          return {}
-        end,
-        ["*"] = function()
-          return {}
-        end,
-      },
-    }
+
+  if is_remote or in_container then
+    if vim.env.TMUX then
+      vim.g.clipboard = {
+        name = "OSC 52 + tmux",
+        copy = {
+          ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+          ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        },
+        paste = {
+          ["+"] = { "tmux", "save-buffer", "-" },
+          ["*"] = { "tmux", "save-buffer", "-" },
+        },
+      }
+    else
+      vim.g.clipboard = {
+        name = "OSC 52 (copy only)",
+        copy = {
+          ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+          ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        },
+        paste = {
+          ["+"] = function()
+            return {}
+          end,
+          ["*"] = function()
+            return {}
+          end,
+        },
+      }
+    end
   else
     vim.opt.clipboard = "unnamedplus"
   end
